7ea035beb8153b87b4d21bd62f380f93ae7051e7
osparking <jbpark03@gmail.com>
2023-06-03 09:22:19 +0900

    initial - except pom.xml

---
+++ src/main/java/space/bumtiger/ss/SsOauthJwtApplication.java
@@ -0,0 +1,13 @@
+package space.bumtiger.ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsOauthJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsOauthJwtApplication.class, args);
+	}
+
+}
